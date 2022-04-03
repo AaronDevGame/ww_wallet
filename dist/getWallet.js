46f9a893ba997dc8f8a7a2430fffd558907646c7
AaronDevGame <aarondevgame@gmail.com>
2022-04-03 09:22:08 +0800

    Update getWallet.js

--- dist/getWallet.js
+++ dist/getWallet.js
@@ -4,15 +4,13 @@ console.log("WALLET HERE");
 // window.cardano.isEnabled();
 
 //popout nami wallet
-// window.cardano.enable();
+window.cardano.enable();
 
 if(window.cardano.isEnabled()) {
     console.log("\n cardano is enabled");
     
     // console.log("window.wallet_address = " + window.wallet_address);
-
     // window.hasOwnProperty('wallet_address');
-
     // +window.wallet_address
 
     // console.log("window cardano = " + window.cardano);
@@ -23,13 +21,9 @@ if(window.cardano.isEnabled()) {
     // }  else {
     //     console.log('no wallet: '+window.wallet_address);
     // }
-    
-
-
 
 } else {
     console.log("\n cardano is not enabled");
-
 }
 
 let x;
@@ -37,15 +31,15 @@ let y = [];
 setTimeout(function () {
     if(window.hasOwnProperty('wallet_address')) {
         console.log('\n wallet address is: '+window.wallet_address);
-        x = window.owned_nfts;
-        console.log('ok NFT x = : '+ x);
-        console.log("type = " + typeof x);
-        y.push(x);
-        console.log('ok NFT y = : '+ y);
+//         x = window.owned_nfts;
+//         console.log('ok NFT x = : '+ x);
+//         console.log("type = " + typeof x);
+//         y.push(x);
+//         console.log('ok NFT y = : '+ y);
 
-        console.log("type = " + typeof y);
-        console.log("type 0 = " + typeof y[0]);
-        console.log("YEHEY!");
+//         console.log("type = " + typeof y);
+//         console.log("type 0 = " + typeof y[0]);
+//         console.log("YEHEY!");
     } else {
         console.log('\n no wallet address');
     }
